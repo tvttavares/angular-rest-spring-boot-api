31aa4dbe31d5efc725a52c6c3feec52e33149621
thiago <tvttavares@gmail.com>
2019-12-09 10:02:27 -0300

    Usando eventos para adicionar header Location

--- 3.12 Usando eventos para adicionar header Location/algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ 3.12 Usando eventos para adicionar header Location/algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,7 +1,5 @@
 package com.example.algamoney.api.resource;
 
-import java.util.Optional;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -26,23 +24,21 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
-
+	
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
-
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
-
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
-		Optional<Pessoa> pessoa = this.pessoaRepository.findById(codigo);
-		return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
+		Pessoa pessoa = pessoaRepository.findOne(codigo);
+		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
 	}
 
 }

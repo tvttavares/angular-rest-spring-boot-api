55098a9d2ba508d721a244f67cf6ba84e3131ea3
thiago <tvttavares@gmail.com>
2019-12-11 09:43:39 -0300

    Atualizando pessoa com PUT

--- 4.3 Atualizando pessoa com PUT/algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ 4.3 Atualizando pessoa com PUT/algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,7 +1,5 @@
 package com.example.algamoney.api.resource;
 
-import java.util.Optional;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -30,10 +28,10 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
-
+	
 	@Autowired
-	private PessoaService PessoaService;
-
+	private PessoaService pessoaService;
+	
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
@@ -41,27 +39,25 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
-
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
-		Optional<Pessoa> pessoa = this.pessoaRepository.findById(codigo);
-
-		return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
+		Pessoa pessoa = pessoaRepository.findOne(codigo);
+		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
 	}
-
+	
 	@DeleteMapping("/{codigo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long codigo) {
-		pessoaRepository.deleteById(codigo);
+		pessoaRepository.delete(codigo);
 	}
-
+	
 	@PutMapping("/{codigo}")
-	public ResponseEntity<Pessoa> atualziar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
-		Pessoa pessoaSalva = PessoaService.atualizar(codigo, pessoa);
-
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
 		return ResponseEntity.ok(pessoaSalva);
 	}
+
 }

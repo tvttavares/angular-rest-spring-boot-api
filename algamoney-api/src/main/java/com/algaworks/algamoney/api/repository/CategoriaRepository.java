68b246d9129650a7c31c000e355661b908902aea
thiago <tvttavares@gmail.com>
2020-02-16 21:06:14 -0400

    Adicionando permissões de acesso

---
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.algaworks.algamoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algamoney.api.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
